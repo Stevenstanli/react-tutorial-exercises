52008d94a6bcdad0bc235f11a7314001ab3c0019
Steven <stanli-peter19@hotmail.com>
2021-02-01 22:46:08 +0000

    Practica React

--- exercises/01.4-building-a-layout/index.js
+++ exercises/01.4-building-a-layout/index.js
@@ -12,9 +12,16 @@ const data = {
 	}
 };
 
-/**
- * define the variable 'content' here and fill it with the
- * needed code to render the bootstrap card
- **/
-
-ReactDOM.render(content, document.querySelector("#myDiv"));
+const output = (
+	<div className="card m-5">
+		<img className="card-img-top" src={data.image} alt="Card image cap" />
+		<div className="card-body">
+			<h5 className="card-title">{data.cardTitle}</h5>
+			<p className="card-text">{data.cardDescription}</p>
+			<a href={data.button.url} className="btn btn-primary">
+				{data.button.label}
+			</a>
+		</div>
+	</div>
+);
+ReactDOM.render(output, document.querySelector("#myDiv"));
